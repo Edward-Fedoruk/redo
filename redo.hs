c9bf04e2d5dca32ed311c32f6676ec8177a33ea8
Eduard Fedoruk <ed.fedorukk@gmail.com>
2020-04-13 21:12:36 +0200

    add some stuff

--- redo.hs
+++ redo.hs
@@ -1,5 +1,8 @@
 import System.Environment (getArgs)
 import System.Process (createProcess, waitForProcess, shell)
+import System.Directory (renameFile, removeFile)
+import System.Exit (ExitCode(..))
+import System.IO (hPutStrLn, stderr)
 
 main :: IO ()
 main = do 
@@ -8,6 +11,10 @@ main = do
   
 redo :: String -> IO ()
 redo target = do
-  (_, _, _, ph) <- createProcess $ shell $ "sh " ++ target ++ ".do"
-  _ <- waitForProcess ph
-  return ()
+  let tmp = target ++ "---redoing"
+  (_, _, _, ph) <- createProcess $ shell $ "sh " ++ target ++ ".do - - " ++ tmp ++ " > " ++ tmp
+  exit <- waitForProcess ph
+  case exit of 
+    ExitSuccess -> do renameFile tmp target
+    ExitFailure code -> do hPutStrLn stderr $ "Redo script exited with non zero exit code: " ++ show code
+                           removeFile tmp
