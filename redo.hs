120ef16415827ba85317c15aad562b43b9f4c63e
Eduard Fedoruk <ed.fedorukk@gmail.com>
2020-04-19 20:11:09 +0200

    refactored code

--- redo.hs
+++ redo.hs
@@ -1,34 +1,31 @@
 import Control.Monad (filterM)
+import Data.Maybe (listToMaybe)
 import System.Environment (getArgs)
 import System.Process (createProcess, waitForProcess, shell)
 import System.Directory (renameFile, removeFile, doesFileExist)
-import System.FilePath (hasExtension, replaceBaseName)
+import System.FilePath (hasExtension, replaceBaseName, takeBaseName)
 import System.Exit (ExitCode(..))
 import System.IO (hPutStrLn, stderr)
 
 main :: IO ()
-main = do 
-  args <- getArgs
-  mapM_ redo args
+main = mapM_ redo =<< getArgs 
   
 redo :: String -> IO ()
-redo target = do
-  let tmp = target ++ "---redoing"
-  maybePath <- redoPath target
-  case maybePath of
-    Nothing -> error $ "file not found '" ++ target ++ "'"
-    Just path -> do
-      (_, _, _, ph) <- createProcess $ shell $ "sh " ++ path ++ " - - " ++ tmp ++ " > " ++ tmp
-      exit <- waitForProcess ph
-      case exit of 
-        ExitSuccess -> do renameFile tmp target
-        ExitFailure code -> do hPutStrLn stderr $ "Redo script exited with non zero exit code: " ++ show code
-                               removeFile tmp
-      
-redoPath :: FilePath -> IO (Maybe FilePath)
-redoPath target = do
-  fmap safeHead $ filterM doesFileExist candidates
-  where candidates = [target ++ ".do"] ++ if hasExtension target then [replaceBaseName target "default" ++ ".do"] else []
+redo target = maybe printMissing redo' =<< redoPath target
+  where redo' :: FilePath -> IO ()
+        redo' path = do
+          (_, _, _, ph) <- createProcess $ shell $ cmd path
+          exit <- waitForProcess ph
+          case exit of 
+            ExitSuccess -> do renameFile tmp target
+            ExitFailure code -> do hPutStrLn stderr $ "Redo script exited with non zero exit code: " ++ show code
+                                   removeFile tmp
+        tmp = target ++ "---redoing"
+        printMissing = error $ "file not found '" ++ target ++ "'"
+        cmd path = unwords ["sh", path, "0", takeBaseName target, tmp, ">", tmp]
 
-safeHead [] = Nothing
-safeHead (x:_) = Just x 
+redoPath :: FilePath -> IO (Maybe FilePath)
+redoPath target = listToMaybe <$> filterM doesFileExist candidates
+  where candidates = [target ++ ".do"] ++ if hasExtension target 
+                                          then [replaceBaseName target "default" ++ ".do"] 
+                                          else []
